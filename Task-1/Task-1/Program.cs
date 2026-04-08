08eb1a7c37a539227115824c20c5c7e1cc1c6a4a
mdabucse <mdabucse@gmail.com>
2026-04-08 09:26:18 +0530

    added task-1[ADD]

---
+++ Task-1/Task-1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a  positive number: ");
+        int n = int.Parse(Console.ReadLine());
+        if (n > 0)
+        {
+            int fact = 1;
+            for(int i = 1; i <= n; i++)
+            {
+                fact *= i;
+            }
+            Console.WriteLine("The Factorial Value is " + fact);
+        }
+
+    }
+}
